d751c87e9206ac6396921e00e5863487d21a04a0
adilet aktay <adiletaktay@gmail.com>
2024-12-16 18:42:50 +0500

    objects as parameters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -58,5 +58,14 @@ public class Main {
         monster1.voice();
         monster2.voice(3);
         monster3.voice(5, "Я монстр 3");
+
+        Box box1 = new Box(10);
+        Box box2 = new Box(20, 20, 10);
+        Box box3 = box1.sumBox(box2);
+        box1.showVolume();
+        box2.showVolume();
+        box3.showVolume();
+        double volumeSum = box1.volume() + box3.volume();
+        System.out.println("box1 + box3 = " + volumeSum);
     }
 }
