313de5ab207ce6784c514d51b51a20c38f88914e
adilet aktay <adiletaktay@gmail.com>
2024-12-19 18:28:19 +0500

    collection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> namesAndNumbers = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
+        }
+        names.add("Андрей");
+        names.add("Катя");
+        names.add("Настя");
+        names.add("Антон");
+        names.add("Женя");
+        names.add("Дима");
+        names.add("Коля");
+        names.add("Мария");
+        names.add("Роман");
+        names.add("Павел");
+        for (int i = 0; i < numbers.size(); i++) {
+            String s = numbers.get(i) + " - " + names.get(i);
+            namesAndNumbers.add(s);
+        }
+        for (String s : namesAndNumbers) {
+            System.out.println(s);
+        }
+
         String sourceData = "Это Иван. Ему 34 года. Его рост 166.3 см. Должность \"Программист\"";
         String name = sourceData.substring(4, 8);
         String ageString = sourceData.substring(14, 16);
