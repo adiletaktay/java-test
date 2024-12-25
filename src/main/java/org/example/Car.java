3bf00d65d788f7dae5d132d8e89da1be22edb281
adilet aktay <adiletaktay@gmail.com>
2024-12-25 18:09:18 +0500

    arraylist

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,47 +2,19 @@ package org.example;
 
 public class Car {
 
-    private String owner;
-    private String manufacturer;
-    private String color;
-    private Engine engine;
+    private String brand;
+    private int number;
 
-    public Car(String owner, String manufacturer, String color) {
-        this.owner = owner;
-        this.manufacturer = manufacturer;
-        this.color = color;
+    public Car(String brand, int number) {
+        this.brand = brand;
+        this.number = number;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if(this == obj) {
-            return true;
-        }
-        if(obj == null) {
-            return false;
-        }
-        if(getClass() != obj.getClass()) {
-            return false;
-        }
-        Car other = (Car) obj;
-
-        if (!manufacturer.equals(other.manufacturer)) {
-            return false;
-        }
-        if (!engine.equals(other.engine)) {
-            return false;
-        }
-        if (!color.equals(other.color)) {
-            return false;
-        }
-        return true;
+    public String getBrand() {
+        return brand;
     }
 
-    @Override
-    public int hashCode() {
-        int result = 1;
-        result = 31 * result + manufacturer.hashCode();
-        result = 31 * result + engine.hashCode();
-        return 31 * result + color.hashCode();
+    public int getNumber() {
+        return number;
     }
 }
