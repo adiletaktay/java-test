3cb977d4996a743cdf484a396e9438a929194dfa
adilet aktay <adiletaktay@gmail.com>
2025-01-18 17:29:40 +0500

    dependency injection

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Cat implements Pet {
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Meow-Meow");
