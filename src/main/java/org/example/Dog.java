77034312960b0a02f4cf99b5335839b28e821450
adilet aktay <adiletaktay@gmail.com>
2025-01-20 16:57:42 +0500

    bean scope init destroy

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Dog implements Pet {
+//    private String name;
+
     public Dog() {
         System.out.println("Dog bean is created");
     }
@@ -9,4 +11,20 @@ public class Dog implements Pet {
     public void say() {
         System.out.println("Bow-Wow");
     }
+
+    protected void init() {
+        System.out.println("Class Dog: init method");
+    }
+
+    private void destroy() {
+        System.out.println("Class Dog: destroy method");
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
