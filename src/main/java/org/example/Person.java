eda37aa23d900beea010ebe8ad830df9ba3391b3
adilet aktay <adiletaktay@gmail.com>
2025-01-21 17:36:22 +0500

    annotation autowired

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
+    @Autowired
     private Pet pet;
     private String surname;
     private int age;
 
+//    @Autowired
 //    public Person(Pet pet) {
 //        System.out.println("Person bean is created");
 //        this.pet = pet;
@@ -14,6 +20,7 @@ public class Person {
         System.out.println("Person bean is created");
     }
 
+    //    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
