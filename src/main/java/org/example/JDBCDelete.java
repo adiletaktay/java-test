7734479ada571aba6f159332317a5defb51d53de
adilet aktay <adiletaktay@gmail.com>
2025-02-03 19:03:52 +0500

    jdbc update select delete

---
+++ src/main/java/org/example/JDBCDelete.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.sql.*;
+
+public class JDBCDelete {
+
+    static final String DB_URl = "jdbc:mysql://localhost:3306/test_db";
+    static final String USER = "jpauser";
+    static final String PWD = "jpapwd";
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(DB_URl, USER, PWD);
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM students WHERE surname = ?");
+            statement.setString(1, "Tregulov");
+            int deletedRows = statement.executeUpdate();
+            System.out.println("deletedRows = " + deletedRows);
+
+            statement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/JDBCUpdate.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class JDBCUpdate {
+
+    static final String DB_URl = "jdbc:mysql://localhost:3306/test_db";
+    static final String USER = "jpauser";
+    static final String PWD = "jpapwd";
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(DB_URl, USER, PWD);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter name:");
+            String enteredName = scanner.nextLine();
+
+//            Statement statement = connection.createStatement();
+//            String sqlQuery = "UPDATE students SET avg_grade = 7.5 " + "WHERE name = '" + enteredName + "'";
+//            statement.executeUpdate(sqlQuery);
+//
+//            statement.close();
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "UPDATE students SET avg_grade = 7.5 WHERE name = ?");
+            statement.setString(1, enteredName);
+            statement.executeUpdate();
+
+            statement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
