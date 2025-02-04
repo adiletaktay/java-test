0a94d3c774bf8cc042dbc5febb0c6fe0653e7258
adilet aktay <adiletaktay@gmail.com>
2025-02-04 18:15:53 +0500

    jdbc insert get id

---
+++ src/main/java/org/example/JDBCInsert_v3.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.sql.*;
+
+public class JDBCInsert_v3 {
+
+    static final String DB_URl = "jdbc:mysql://localhost:3306/test_db";
+    static final String USER = "jpauser";
+    static final String PWD = "jpapwd";
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Student student = new Student("Isaac", "Sharp", 9.8);
+
+        try {
+            connection = DriverManager.getConnection(DB_URl, USER, PWD);
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO students(name, surname, avg_grade) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, student.getName());
+            statement.setString(2, student.getSurName());
+            statement.setDouble(3, student.getAvgGrade());
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Failed to add Student to DB");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                student.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("Failed to create Student ID");
+            }
+
+            System.out.println(student);
+
+            statement.close();
+            generatedKeys.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
